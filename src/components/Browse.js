ce75958fca58fa81fe860ba1206429bcd433a01b
Chanaka Mohottalalage <chanaka.mohottalalage@datacom.com>
2025-02-24 08:46:04 +1300

    rollback the key

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -5,6 +5,14 @@ const Browse = () => {
   return (
     <div>
       <Header></Header>
+
+      
+      <div className='py-20 flex justify-between'>
+        <div className='flex-auto'>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+      </div>
+
     </div>
   )
 }
